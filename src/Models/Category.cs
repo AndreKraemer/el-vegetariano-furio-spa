0a5f9d3ff0aea397b1088bb14753d3d89e4ac8ab
André Krämer <andre@andrekraemer.de>
2019-10-24 13:04:13 +0200

    Added Models folder

---
+++ src/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ElVegetarianoFurio.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            Dishes = new HashSet<Dish>();
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<Dish> Dishes { get; set; }
+    }
+}
---
+++ src/Models/Dish.cs
@@ -0,0 +1,12 @@
+﻿namespace ElVegetarianoFurio.Models
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
