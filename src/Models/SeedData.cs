1ff71d2e89521938bb99c46a397790a437dc0218
André Krämer <andre@andrekraemer.de>
2019-10-25 23:39:50 +0200

    Seeded Database

---
+++ src/Models/SeedData.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElVegetarianoFurio.Repositories;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ElVegetarianoFurio.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var vegiContext = new VegiContext(
+                serviceProvider.GetRequiredService<DbContextOptions<VegiContext>>()))
+            {
+                if (vegiContext.Dishes.Any())
+                {
+                    return;
+                }
+
+                var categoriesRepository =
+                    new FileCategoryRepository(serviceProvider.GetRequiredService<IWebHostEnvironment>());
+
+                var categories = categoriesRepository.GetCategories().ToList();
+                categories.ForEach(x =>
+                {
+                    x.Id = 0;
+                    foreach (var dish in x.Dishes)
+                    {
+                        dish.Id = 0;
+                        dish.CategoryId = 0;
+                    }
+                });
+
+                vegiContext.Categories.AddRange(categories);
+                vegiContext.SaveChanges();
+
+            }
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ElVegetarianoFurio.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace ElVegetarianoFurio
@@ -13,7 +15,21 @@ namespace ElVegetarianoFurio
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured seeding the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
